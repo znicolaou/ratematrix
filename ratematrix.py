dbf4b89209da4c24293913177adf254d82411d4c
Zachary Nicolaou <zack@Zactop.local>
2019-05-11 15:43:25 -0500

    Created plots, validated, and debugged

--- ratematrix.py
+++ ratematrix.py
@@ -9,13 +9,16 @@ import argparse
 from scipy.sparse import coo_matrix
 from scipy.sparse.linalg import eigs
 from scipy.special import factorial
+import sys
 
 #Command-line arguments
 parser = argparse.ArgumentParser(description='Generate a sparse rate matrix from cantera model.')
 parser.add_argument("--filebase", type=str, required=True, dest='filebase', help='Base string for npy file output. Three files will be created for each reaction, storing rates, row indices, and column indices.')
 parser.add_argument("--mechanism", type=str, required=False, default='mechanisms/h2o2.cti', dest='mechanism', help='Mechanism cti file. Default mechanisms/h2o2.cti.')
-parser.add_argument("--calculate", type=int, required=False, default=1, choices=[0,1], help='Flag to calculate rate matrix and eigenvalues. If 1, calculate space, then print dimension, runtime, sparsity, and three smallest eigenvalues, save rate matrix, then eigenvalues, and eigenvectors, then quit. If 0, only calculate space, then print dimension, runtime, recursive calls, and recursive levels, then quit. Default 1.')
-parser.add_argument("--plot", type=int, required=False, default=1, choices=[0,1], help='Flag to plot eigenvalues. Default 1.')
+parser.add_argument("--calculate", type=int, required=False, default=1, choices=[0,1], help='Flag to calculate rate matrix and eigenvalues. If 1, calculate space, then print args.temperature, args.pressure, total atoms, dimension, runtime, recursive calls, recursive levels, sparsity, and Nvals largest eigenvalues, save rate matrix, eigenvalues, and eigenvectors, then quit. If 0, only calculate space, then print total atoms, dimension, runtime, recursive calls, and recursive levels, then quit. Default 1.')
+parser.add_argument("--Nvals", type=int, required=False, default=-1, help='Number of eigenvalues to print; 0 for all. Default 0.')
+parser.add_argument("--plot", type=int, required=False, default=1, choices=[0,1], help='Flag to plot eigenvalues and save filebase/eigenvales.pdf. Default 1.')
+parser.add_argument("--save", type=int, required=False, default=1, choices=[0,1], help='Flag to save the results to filebaserate.npy, filebaseeigenvalues.npy, and filebaseeigenvectors.npy. Default 1.')
 parser.add_argument("--temperature", type=float, required=False, default=1500, help='Temperature in Kelvin. Default 1500.')
 parser.add_argument("--pressure", type=float, required=False, default=1, help='Pressure in atm. Default 1.')
 parser.add_argument("--atoms", nargs='+', type=int, required=False, default=[3, 3, 3], help='Number of each atom, in order of their appearance in the .cti file. If number of values is not number of atoms, print the atoms. Default 3 3 3.')
@@ -25,7 +28,7 @@ args = parser.parse_args()
 def get_multiindex(index):
     return multiindices[index]
 def get_index(multiindex):
-    return np.where(multiindices==multiindex)[0][0]
+    return np.where(np.all(multiindices==multiindex,axis=1))[0][0]
 
 #Function to get the transition rate given concentrations and rate constant
 def get_rate (multiindex, rstoi, k, reaction):
@@ -151,8 +154,12 @@ for i in range(ns):
 multiindices,count,level=recursive_list(atoms,multiindex,last_avail)
 dim=len(multiindices)
 if args.calculate==0:
-    #Print dimension, runtime, recursive calls, and recursive levels
-    print(dim, timeit.default_timer()-start, count, level)
+    #Print total atoms, dimension, runtime, recursive calls, and recursive levels
+    print(np.sum(atoms), dim, timeit.default_timer()-start, count, level)
+    out=open(filebase+"out.dat","a+")
+    print(np.sum(atoms), dim, timeit.default_timer()-start, count, level,file=out)
+    out.close()
+    sys.stdout.flush()
     quit()
 
 #Loop through each reaction index and calculate spase elements
@@ -166,13 +173,18 @@ for rind in range(nr):
     columns+=reac_columns
 nonzero=np.array([rows,columns])
 ratematrix=coo_matrix((np.array(data),(np.array(rows),np.array(columns))),(dim,dim))
-np.save(filebase+"rate.npy",ratematrix.toarray())
 #The dimension is not that big - don't use spase matrix algorithms for eigenvalues
 eigenvalues,eigenvectors=np.linalg.eig(ratematrix.toarray())
-np.save(filebase+"eigenvalues.npy",eigenvalues)
-np.save(filebase+"eigenvectors.npy",eigenvectors)
+if args.save==1:
+    np.save(filebase+"ratematrix.npy",ratematrix.toarray())
+    np.save(filebase+"eigenvalues.npy",eigenvalues)
+    np.save(filebase+"eigenvectors.npy",eigenvectors)
 #Print dimension, runtime, sparsity, and three smallest eigenvalues
-print(dim, timeit.default_timer()-start, (1-len(np.unique(nonzero))*1.0/(dim)**2), *eigenvalues[-3:])
+print(args.temperature, args.pressure, np.sum(atoms), dim, timeit.default_timer()-start, count, level, (1-len(np.unique(nonzero))*1.0/(dim)**2), *np.sort(eigenvalues[-args.Nvals:]))
+out=open(filebase+"out.dat","a+")
+print(args.temperature, args.pressure, np.sum(atoms), dim, timeit.default_timer()-start, count, level, (1-len(np.unique(nonzero))*1.0/(dim)**2), *np.sort(eigenvalues[-args.Nvals:]),file=out)
+out.close()
+sys.stdout.flush()
 
 if args.plot==1:
     fig=plt.figure()
---
+++ scripts/sweep.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+N_temp=10
+N_press=10
+press0=0.5
+press1=10
+temp0=1000
+temp1=2500
+atoms="5 5 5"
+filebase0=data/sweep
+save=0
+
+if [ -f ${filebase0}out.dat ]; then rm ${filebase0}out.dat; fi
+for i in `seq 0 $N_temp`; do
+  for j in `seq 0 $N_press`; do
+    temp=`bc -l <<<"${temp0}+(${temp1}-${temp0})*${i}*1.0/${N_temp}"`
+    press=`bc -l <<<"${press0}+(${press1}-${press0})*${j}*1.0/${N_press}"`
+    filebase=${filebase0}/${i}/${j}/
+    mkdir -p $filebase
+    echo $temp $press
+    echo "./ratematrix.py --temperature $temp --pressure $press --atoms $atoms --filebase ${filebase} --plot 0 --save $save"
+    ./ratematrix.py --temperature $temp --pressure $press --atoms $atoms --filebase ${filebase} --plot 0 --save $save &
+  done
+  wait
+
+  for j in `seq 0 $N_press`; do
+    filebase=${filebase0}/${i}/${j}/
+    cat ${filebase}out.dat >> ${filebase0}out.dat
+    rm ${filebase}out.dat
+    rmdir $filebase
+  done
+  rmdir ${filebase0}/${i}
+
+done
+rmdir ${filebase0}
