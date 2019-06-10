99ab07625eec0f4d4f6af79bcb78d5ad3432dc2a
znicolaou <zack.nicolaou@gmail.com>
2019-06-10 16:36:24 -0500

    Logarithmic pressure sweep

--- scripts/sweep.sh
+++ scripts/sweep.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-N_temp=10
-N_press=10
-press0=0.1
-press1=2
+N_temp=50
+N_press=50
+press0=0.01
+press1=100
 temp0=600
 temp1=1000
-atoms="6 12 5"
+atoms="4 8 5"
+procs=100
 filebase0=data/sweep
-# save=0
 export OMP_NUM_THREADS=1
 
 rm -r ${filebase0}*
@@ -16,18 +16,21 @@ rm -r ${filebase0}*
 mkdir -p $filebase0
 for i in `seq 0 $N_temp`; do
   for j in `seq 0 $N_press`; do
-    temp=`bc -l <<<"${temp0}+(${temp1}-${temp0})*${i}*1.0/${N_temp}"`
-    press=`bc -l <<<"${press0}+(${press1}-${press0})*${j}*1.0/${N_press}"`
+    js=`jobs | wc -l`
+    while [ $js -ge $procs ]; do
+      js=`jobs | wc -l`
+      sleep 1
+    done
+    temp=`bc -l <<<"${temp0}+(${temp1}-${temp0})*${j}*1.0/${N_temp}"`
+    press=`bc -l <<<"${press0}*e(${i}*1.0/${N_press}*l(${press1}/${press0}))"`
     echo $temp $press
     filebase=$filebase0/${i}_${j}
     cp ${filebase0}multiindices.npy ${filebase}multiindices.npy
     cp ${filebase0}out.dat ${filebase}out.dat
     ./ratematrix.py --temperature $temp --pressure $press --atoms $atoms --filebase ${filebase} --accumulate 1 --plot 0 --save 1 --calculate 1 --fix 8 5 &
-    sleep 1
   done
-  wait
-  sleep 10
 done
+wait
 
 rm ${filebase0}out.dat
 
