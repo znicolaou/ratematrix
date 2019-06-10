af6d85ce070e66891a40c3f99f74f3870a44bf84
znicolaou <zack.nicolaou@gmail.com>
2019-06-10 16:38:47 -0500

    Parallel gri script

--- scripts/parallelgri.sh
+++ scripts/parallelgri.sh
@@ -26,7 +26,7 @@ for i in `seq 0 $((4*num))`; do
         sleep 0.01
         js=`jobs | wc -l`
       done
-      ./ratematrix.py --filebase ${filebase0}/${i}_${j} --mechanism mechanisms/gri30.cti --atoms $atoms --fix 1 $i 2 $j 3 $k 48 $num --calculate 0 &
+      ./ratematrix.py --filebase ${filebase0}/${i}_${j}_${k} --mechanism mechanisms/gri30.cti --atoms $atoms --fix 1 $i 2 $j 4 $k 48 $num --calculate 0 &
     done
   done
 done
@@ -36,8 +36,8 @@ wait
 for i in `seq 0 $((4*num))`; do
     for j in `seq 0 $((4*num))`; do
       for k in `seq 0 $((4*num))`; do
-      cat ${filebase0}/${i}_${j}out.dat >> ${filebase0}pout.dat
-      # ${filebase0}/${i}_${j}out.dat
+      cat ${filebase0}/${i}_${j}_${k}out.dat >> ${filebase0}pout.dat
+      rm ${filebase0}/${i}_${j}_${k}out.dat
     done
   done
 done
@@ -46,7 +46,7 @@ dim=`awk '{dim+=$2}END{print dim}' ${filebase0}pout.dat`
 count=`awk '{count+=$4}END{print count}' ${filebase0}pout.dat`
 level=`awk '{if($5>max){max=$5}}END{print max}' ${filebase0}pout.dat`
 echo "$((12*num)) $dim $((end-start)) $count $level" >> ${filebase0}out.dat
-rm ${filebase0}pout.dat
+# rm ${filebase0}pout.dat
 
-./ratematrix.py --filebase ${filebase0} --mechanism mechanisms/gri30.cti --atoms $atoms --fix 1 $i 2 $j 3 $k 48 $num --calculate 0 --accumulate 1
+./ratematrix.py --filebase ${filebase0} --mechanism mechanisms/gri30.cti --atoms $atoms --fix 1 $i 2 $j 4 $k 48 $num --calculate 0 --accumulate 1
 # rm -r $filebase0
