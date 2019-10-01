145c4555d3e6df65b2a6fc6bef901a1d7127ea68
znicolaou <zack.nicolaou@gmail.com>
2019-10-01 11:52:58 -0500

    updated plots

--- combustion.py
+++ combustion.py
@@ -45,13 +45,13 @@ def runsim (times):
 
 parser = argparse.ArgumentParser(description='Generate a sparse rate matrix from cantera model.')
 parser.add_argument("--filebase", type=str, required=True, dest='filebase', help='Base string for npy file output. Three files will be created for each reaction, storing rates, row indices, and column indices.')
-parser.add_argument("--temperature", type=float, required=False, default=1500, help='Temperature in Kelvin. Default 1500.')
-parser.add_argument("--adiabatic", type=int, choices=[0, 1, 2], required=False, default=0, help='Convert energy from reactions to heat. The values 0, 1, and 2 correspond to constant volume/temperature, constant volume/energy, and constant pressure/enthalpy, respectively. The temperature is specify the reference multiindix specified with --reference. ')
-parser.add_argument("--reference", type=int, nargs='+', required=False, default=[0, 4, 3, 2, 8, 0], help='Reference multiindex for which the temperature and number of atoms are specified. Default 0 4 3 2 8 0.')
+parser.add_argument("--temperature", type=float, required=False, default=1000, help='Temperature in Kelvin. Default 1000.')
+parser.add_argument("--adiabatic", type=int, choices=[0, 1, 2], required=False, default=1, help='Convert energy from reactions to heat. The values 0, 1, and 2 correspond to constant volume/temperature, constant volume/energy, and constant pressure/enthalpy, respectively. The temperature is specify the reference multiindix specified with --reference. ')
+parser.add_argument("--reference", type=int, nargs='+', required=False, default=[0, 10, 3, 5, 8, 100], help='Reference multiindex for which the temperature and number of atoms are specified. Default 0 4 3 2 8 0.')
 parser.add_argument("--pressure", type=float, required=False, default=1, help='Pressure in atm. Default 1.')
 parser.add_argument("--t0", type=float, required=False, default=1e-8, help='Initial integration time for propogating.')
-parser.add_argument("--tmax", type=float, required=False, default=1e-2, help='Final integration time for propogating.')
-parser.add_argument("--Npoints", type=int, required=False, default=25, help='Number of times to propogate.')
+parser.add_argument("--tmax", type=float, required=False, default=1, help='Final integration time for propogating.')
+parser.add_argument("--Npoints", type=int, required=False, default=100, help='Number of times to propogate.')
 parser.add_argument("--sensitivities", type=int, required=False, choices=[0,1],default=1, help='Flag to calculate sensitivities.')
 args = parser.parse_args()
 
@@ -84,27 +84,34 @@ times=[t0*(tmax/t0)**(n*1.0/Npoints) for n in range(Npoints)]
 
 observation=runsim(times)
 
-species = gas.species_names
-reactions = gas.reactions()
-f=open(out,'w')
-outarray=[]
-outarray.append(observation.t)
-outarray.append(observation.T)
-outarray.append((observation.P/ct.one_atm))
-columns='t,T,P'
-for j in range(0, gas.n_total_species):
-    columns=columns + ',[%s]'%(gas.species_names[j])
-    outarray.append([item for sublist in observation(gas.species_names[j]).X for item in sublist])
-for j in range(0, gas.n_reactions):
-    columns=columns + ',%s Flux'%(str(reactions[j].reactants).replace(" ","").replace(",","+").replace("{","").replace("}","")+"->"+str(reactions[j].products).replace(" ","").replace(",","+").replace("{","").replace("}",""))
-    outarray.append(np.transpose(observation.net_rates_of_progress)[j])
-if(sensflag==1):
-    for j in range(0, gas.n_total_species):
-        for k in range(0, gas.n_reactions):
-            columns=columns + ',%s %s-Sensitivity'%(str(reactions[k].reactants).replace(" ","").replace(",","+").replace("{","").replace("}","")+"->"+str(reactions[k].products).replace(" ","").replace(",","+").replace("{","").replace("}",""), gas.species_names[j])
-            outarray.append(np.array(observation.sens).transpose()[j*gas.n_reactions+k])
 
-np.savetxt(out, np.transpose(outarray), delimiter=',', header=columns, comments='')
+np.save(args.filebase+"times.npy", observation.t)
+np.save(args.filebase+"concentrations.npy", observation.X)
+np.save(args.filebase+"temperatures.npy", observation.T)
+np.save(args.filebase+"pressures.npy", observation.P/ct.one_atm)
+
+# species = gas.species_names
+# reactions = gas.reactions()
+# f=open(out,'w')
+# outarray=[]
+# outarray.append(observation.t)
+# outarray.append(observation.T)
+# outarray.append((observation.P/ct.one_atm))
+# columns='t,T,P'
+#
+# for j in range(0, gas.n_total_species):
+#     columns=columns + ',[%s]'%(gas.species_names[j])
+#     outarray.append([item for sublist in observation(gas.species_names[j]).X for item in sublist])
+# for j in range(0, gas.n_reactions):
+#     columns=columns + ',%s Flux'%(str(reactions[j].reactants).replace(" ","").replace(",","+").replace("{","").replace("}","")+"->"+str(reactions[j].products).replace(" ","").replace(",","+").replace("{","").replace("}",""))
+#     outarray.append(np.transpose(observation.net_rates_of_progress)[j])
+# if(sensflag==1):
+#     for j in range(0, gas.n_total_species):
+#         for k in range(0, gas.n_reactions):
+#             columns=columns + ',%s %s-Sensitivity'%(str(reactions[k].reactants).replace(" ","").replace(",","+").replace("{","").replace("}","")+"->"+str(reactions[k].products).replace(" ","").replace(",","+").replace("{","").replace("}",""), gas.species_names[j])
+#             outarray.append(np.array(observation.sens).transpose()[j*gas.n_reactions+k])
+#
+# np.savetxt(out, np.transpose(outarray), delimiter=',', header=columns, comments='')
 
 stop=timeit.default_timer()
 print ('runtime: %f'%(stop-start))
