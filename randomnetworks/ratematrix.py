484854f4621dead61bfb8c7ec030d676f508ac34
znicolaou <zack.nicolaou@gmail.com>
2019-10-11 09:22:12 -0500

    update random'

--- randomnetworks/ratematrix.py
+++ randomnetworks/ratematrix.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 import os
 import numpy as np
-import cantera as ct
+# import cantera as ct
 import timeit
 import argparse
-from scipy.sparse import coo_matrix, save_npz, load_npz
+import resource
+from scipy.sparse import coo_matrix
 from scipy.sparse.linalg import eigs, svds
 from scipy.special import factorial, binom
 from scipy.integrate import ode
@@ -15,13 +16,13 @@ import glob
 #Command-line arguments
 parser = argparse.ArgumentParser(description='Generate a sparse rate matrix from cantera model.')
 parser.add_argument("--filebase", type=str, required=True, dest='filebase', help='Base string for npy file output. Three files will be created for each reaction, storing rates, row indices, and column indices.')
-parser.add_argument("--mechanism", type=str, required=False, default='mechanisms/h2o2.cti', dest='mechanism', help='Mechanism cti file. Default mechanisms/h2o2.cti.')
+parser.add_argument("--species", type=int, required=False, default=9, help='Number of species.')
+parser.add_argument("--reactions", type=int, required=False, default=21, help='Number of reactions.')
+parser.add_argument("--molecules", type=int, required=False, default=1, help='Number of molecules.')
+parser.add_argument("--seed", type=int, required=False, default=1, help='Random seed.')
 parser.add_argument("--calculate", nargs=2, type=int, required=False, default=[0,-1], help='Rows to calculate in rate matrix. Takes starting index and ending index and save rate matrix corresponding to all reactions over these rows. Default [0 -1].')
 parser.add_argument("--eigenvalues", type=int, required=False, default=50, help='Flag to calculate  eigenvalues. If 1, then print args.temperature, args.pressure, total atoms, dimension, runtime, recursive calls, recursive levels, and save rate matrix, eigenvalues, and eigenvectors, then quit. Default 1.')
-parser.add_argument("--temperature", type=float, required=False, default=1000, help='Temperature in Kelvin. Default 1500.')
-parser.add_argument("--adiabatic", type=int, choices=[0, 1, 2], required=False, default=1, help='Convert energy from reactions to heat. The values 0, 1, and 2 correspond to constant volume/temperature, constant volume/energy, and constant pressure/enthalpy, respectively. The temperature is specify the reference multiindix specified with --reference. ')
-parser.add_argument("--reference", type=int, nargs='+', required=False, default=[0, 4, 3, 2, 8, 0], help='Reference multiindex for which the temperature and number of atoms are specified. Default 0 4 3 2 8 0.')
-parser.add_argument("--pressure", type=float, required=False, default=1, help='Pressure in atm. Default 1.')
+# parser.add_argument("--reference", type=int, nargs='+', required=False, default=[0, 1, 1, 1], help='Reference multiindex for which the temperature and number of atoms are specified. Default 0 4 3 2 8 0.')
 parser.add_argument("--fix", nargs='+', type=int, required=False, default=[], help='Fix species numbers for parallelization. Include each species index followed by the number of molecules to fix.')
 parser.add_argument("--accumulate", type=int, required=False, default=0, choices=[0,1], help='Flag to accumulate the multiindices from parallel runs.')
 parser.add_argument("--propogate", type=int, required=False, default=0, choices=[0,1], help='Flag to propogate reference multiindex.')
@@ -37,47 +38,25 @@ def get_multiindex(index):
 def get_index(multiindex):
     return np.where(np.all(multiindices==multiindex,axis=1))[0][0]
 
-def get_rate (multiindex, stoi, k, vol):
+def get_rate (multiindex, stoi, k):
 
     if np.all(multiindex>=stoi):
-        return k*np.product(binom(multiindex, stoi)*factorial(stoi))/(ct.avogadro*vol)**(np.sum(stoi)-1)
-        # return k*np.product(factorial(multiindex)/factorial(multiindex-stoi))/(ct.avogadro*vol)**(np.sum(stoi)-1)
+        return k*np.product(binom(multiindex, stoi)*factorial(stoi))
     else:
         return 0.
 
 #We can parallelize this by calculating for each row, which should go relatively fast.
 def calculate_sparse_elements_row(rind,i):
-    reaction=gas.reactions()[rind]
-    rstoi=np.array([reaction.reactants[x] if x in reaction.reactants.keys() else 0 for x in species])
-    pstoi=np.array([reaction.products[x] if x in reaction.products.keys() else 0 for x in species])
+    rstoi=np.zeros(ns)
+    pstoi=np.zeros(ns)
     data=[]
     rows=[]
     columns=[]
     multiindex=get_multiindex(i)
-    if(args.adiabatic == 1):
-        gas.UVX=refenergy/refmass,refvol/refmass,multiindex
-    elif(args.adiabatic == 2):
-        gas.HPX=(refenth+np.dot(multiindex-refmultiindex,refpotentials)/(1000*ct.avogadro))/refmass,args.pressure*ct.one_atm,multiindex
-    else:
-        gas.TPX=args.temperature,args.pressure*ct.one_atm,multiindex
-
-    #forward reaction
-    k=gas.forward_rate_constants[rind]
     multiindex2=multiindex-rstoi+pstoi
+    k=1
     if np.all(multiindex2>=0.) and not np.isnan(k) and (np.any([np.all(multiindex2==multiindex) for multiindex in multiindices])):
-        rate=get_rate(multiindex,rstoi,k,refvol)
-        j=get_index(multiindex2)
-        data.append(rate)
-        rows.append(i)
-        columns.append(j)
-        data.append(-rate)
-        rows.append(i)
-        columns.append(i)
-    #reverse reaction
-    k=gas.reverse_rate_constants[rind]
-    multiindex2=multiindex+rstoi-pstoi
-    if np.all(multiindex2>=0) and not np.isnan(k) and (np.any([np.all(multiindex2==multiindex) for multiindex in multiindices])):
-        rate=get_rate(multiindex,pstoi,k,refvol)
+        rate=get_rate(multiindex,rstoi,k)
         j=get_index(multiindex2)
         data.append(rate)
         rows.append(i)
@@ -101,116 +80,51 @@ def calculate_sparse_elements(rind, startrow, endrow):
 
 #Main
 filebase=args.filebase
-mechanism=args.mechanism
-gas=ct.Solution(mechanism)
-gas.TP=args.temperature,args.pressure*ct.one_atm
-ns=gas.n_species
-nr=gas.n_reactions
-species=gas.species_names
-elements=gas.element_names
+ns=args.species
+nr=args.reactions
+nm=args.molecules
+np.random.seed(args.seed)
 refmultiindex=np.zeros(ns,dtype=int)
-for i in range(0,len(args.reference),2):
-    refmultiindex[args.reference[i]]=args.reference[i+1]
-sp_atoms=[]
-for i in range(ns):
-    sp_atoms.append(np.array([int(gas.species()[i].composition[el] if el in gas.species()[i].composition.keys() else 0) for el in elements]))
-sp_atoms=np.array(sp_atoms)
-atoms=np.zeros(len(elements),dtype=int)
-for i in range(len(refmultiindex)):
-    atoms += refmultiindex[i]*sp_atoms[i]
+# for i in range(0,len(args.reference),2):
+#     refmultiindex[args.reference[i]]=args.reference[i+1]
+for i in range(0,ns):
+    refmultiindex[i]=args.molecules
 fixed=np.array(args.fix)
-
-atoms=np.array(atoms)
-gas.TPX=args.temperature,args.pressure*ct.one_atm,refmultiindex
-refquant=ct.Quantity(gas,moles=np.sum(refmultiindex)/ct.avogadro)
-refenth=refquant.enthalpy
-refentr=refquant.entropy
-refenergy=refquant.int_energy
-refmass=refquant.mass
-refvol=refquant.volume
-refpotentials=refquant.chemical_potentials
 
 #Calculate the space of possible states
 if args.accumulate==0:
     start=timeit.default_timer()
 
-    multiindex=[]
-    last_avail=[[],[]]
-    for i in range(ns):
-        multiindex.append(0)
-        last_avail[0].append(i)
-        last_avail[1].append(np.array([int(gas.species()[i].composition[el] if el in gas.species()[i].composition.keys() else 0) for el in elements]))
-
-    remove_atoms = np.zeros(len(atoms));
-    for i in range(0,len(fixed),2):
-        remove_atoms += fixed[i+1]*sp_atoms[fixed[i]]
-
-    frvecs=gas.reactant_stoich_coeffs()-gas.product_stoich_coeffs()
-    rrvecs=gas.product_stoich_coeffs()-gas.reactant_stoich_coeffs()
-    rvecs=np.column_stack((frvecs,rrvecs))
+    #ensure that the reactions are distinct...
+    rvecs=np.zeros((nr,ns))
+    for i in range(nr):
+        reactants=np.random.choice(np.arange(ns),size=2,replace=False)
+        products=np.random.choice(np.setdiff1d(np.arange(ns),reactants),size=2,replace=False)
+        rvecs[i,reactants]=-1
+        rvecs[i,products]=1
+    # print(rvecs)
     # multiindices,count,level=rlist.list(atoms-remove_atoms.astype(int), sp_atoms, fixed[::2].astype(int))
-    multiindices,count,level=rlist.list(refmultiindex, sp_atoms, fixed[::2].astype(int), rvecs.astype(int))
-
+    multiindices=rlist.list(refmultiindex, fixed[::2].astype(int), rvecs.astype(int))
+    # print(refmultiindex)
+    # print(multiindices)
+    # quit()
     for i in range(0,len(fixed),2):
         multiindices[:,fixed[i]]=fixed[i+1]
 
-    accessible=[]
-    temperatures=[]
-    pressures=[]
-    inaccessible=[]
-    gas=ct.Solution(mechanism)
-
-    if(args.adiabatic == 1):
-        for multiindex in multiindices:
-            try:
-                gas.UVX=refenergy/refmass,refvol/refmass,multiindex
-                quant=ct.Quantity(gas, moles=np.sum(multiindex)/ct.avogadro)
-                if(quant.T > 100): #far outside the range where these constants are valid...
-                    temperatures.append(quant.T)
-                    pressures.append(quant.P/ct.one_atm)
-                    accessible.append(multiindex)
-                else:
-                    inaccessible.append(multiindex)
-            except:
-                inaccessible.append(multiindex)
-    elif(args.adiabatic == 2):
-        for multiindex in multiindices:
-            try:
-                gas.HPX=(refenth+np.dot(multiindex-refmultiindex,refpotentials)/(1000*ct.avogadro))/refmass,args.pressure*ct.one_atm,multiindex
-                quant=ct.Quantity(gas, moles=np.sum(multiindex)/ct.avogadro)
-                if(quant.T > 100): #far outside the range where these constants are valid...
-                    temperatures.append(quant.T)
-                    pressures.append(quant.P/ct.one_atm)
-                    accessible.append(multiindex)
-                else:
-                    inaccessible.append(multiindex)
-            except:
-                inaccessible.append(multiindex)
-    else:
-        accessible=multiindices
-        temperatures=np.zeros(len(multiindices))+args.temperature
-        pressures=np.zeros(len(multiindices))+args.pressure
-
-    multiindices=np.array(accessible)
     dim=len(multiindices)
 
-    atot=np.sum(atoms)
+    # atot=np.sum(atoms)
     runtime=timeit.default_timer()-start
 
 
     np.save(filebase+"multiindices.npy",multiindices)
-    np.save(filebase+"temperatures.npy",temperatures)
-    np.save(filebase+"pressures.npy",pressures)
     out=open(filebase+"out.dat","w")
 
-    print(atot, dim, runtime, count, level, file=out)
+    print(dim, runtime, file=out)
     print(*refmultiindex, file=out)
-    print(*elements, file=out)
     out.close()
     if(args.print == 1):
         print("state space dimension: ", dim)
-        print("state space level: ", level)
-        print("state space calls: ", count)
         print("state space runtime: ", runtime)
 else:
     if os.path.isfile(filebase+"multiindices.npy"):
@@ -238,11 +152,11 @@ else:
 
     file=open(filebase+"out.dat","r")
     instrings=file.readline().split()
-    atot=int(instrings[0])
-    dim=int(instrings[1])
-    runtime=float(instrings[2])
-    count=int(instrings[3])
-    level=int(instrings[4])
+    # atot=int(instrings[0])
+    dim=int(instrings[0])
+    runtime=float(instrings[1])
+    # count=int(instrings[3])
+    # level=int(instrings[4])
     instrings=file.readline().split()
     refmultiindex=np.array([int(float(i)) for i in instrings])
 
@@ -252,14 +166,6 @@ if endrow==-1 or endrow > dim:
 if endrow>startrow:
     #Loop through each reaction index and calculate spase elements
     start=timeit.default_timer()
-
-    gas=ct.Solution(mechanism)
-    gas.TPX=args.temperature,args.pressure*ct.one_atm,refmultiindex
-    refquant=ct.Quantity(gas,moles=np.sum(refmultiindex)/ct.avogadro)
-    refenth=refquant.enthalpy
-    refmass=refquant.mass
-    refvol=refquant.volume
-    quant=ct.Quantity(gas, moles=np.sum(refmultiindex)/ct.avogadro)
 
     data=[]
     rows=[]
@@ -368,30 +274,24 @@ if args.propogate == 1:
 
     def func(t,y):
         return ratematrix.dot(y)
-    # A=ratematrix.toarray()
-    # def jac(t, y):
-        # return A
-    # r=ode(func, jac)
-    r=ode(func)
 
     y0=np.zeros(dim)
-    y0[get_index(refmultiindex)]=1
-    y0 = y0
-
-    # r.set_initial_value(y0).set_integrator('vode', atol=1e-8, rtol=1e-6, method='bdf', first_step=args.t0/1000, nsteps=10000)
-    r.set_initial_value(y0).set_integrator('lsoda', atol=1e-8, rtol=1e-6, first_step=args.t0/100, nsteps=10000)
-
+    y0[get_index(refmultiindex)]=1.0
     times=[args.t0*(args.tmax/args.t0)**(n*1.0/args.Nt) for n in range(args.Nt)]
-    vals=np.zeros((args.Nt,dim))
-    for n in range(len(times)):
-        r.integrate(times[n])
-        vals[n]=r.y
+    sol=solve_ivp(func,[0,args.tmax], y0, method='BDF', t_eval=times, atol=1e-8, rtol=1e-6, first_step=args.t0/100, jac=ratematrix)
+    vals=np.transpose(np.array(sol.y)).tolist()
+
     np.save(filebase+"times.npy",np.array(times))
     np.save(filebase+"propogate.npy",vals)
 
     runtime=timeit.default_timer()-start
     out=open(filebase+"rout.dat","w")
     print(runtime, file=out)
+    print(resource.getrusage(resource.RUSAGE_SELF).ru_maxrss, file=out)
+    print(dim, file=out)
     out.close()
     if(args.print == 1):
+        print("propogate success:", sol.success)
         print("propogate runtime:", runtime)
+if(args.print == 1):
+    print("memory:", resource.getrusage(resource.RUSAGE_SELF).ru_maxrss)
--- randomnetworks/rlistmodule.c
+++ randomnetworks/rlistmodule.c
@@ -13,94 +13,66 @@ static void printMultiindex (Multiindex mult) {
   printf("\n");
 }
 
-static void recursive_list(Multiindex current, MultiindexVector avail, MultiindexVector reactions, MultiindexSet &previously_enumerated, MultiindexVector &ret, long int &count, long int &maxlevel, long int level){
-  //create a stack to hold the list of next states. In each loop, calculate the available reactions and add the next states to the stack. Loop while the stack is not empty.
+static void recursive_list(Multiindex current, MultiindexVector reactions, MultiindexSet &previously_enumerated){
   MultiindexVector stack;
   stack.push_back(current);
   previously_enumerated.insert(current);
+  int remove;
 
   while(!stack.empty()) {
     current=stack.back();
-    // printMultiindex(current);
-    ret.push_back(current);
+    // printf("%i\t%i\t\r",stack.size(),previously_enumerated.size());
     stack.pop_back();
-    count++;
-    //recursive cases
-
-    //Loop through available reactions, create the next multiindex, and recurse if next has not been previously enumerated
-
     for (auto itr = reactions.begin(); itr != reactions.end(); ++itr) {
       Multiindex next(current.na, current.ns);
-      for(int i=0; i<next.ns; i++)
+      remove=0;
+      for(int i=0; i<next.ns; i++){
         next.species[i]=current.species[i]+itr->species[i];
-      //we need to populate the next_avail here on the basis of all reactions, not just the previously available ones
+        if(next.species[i] < 0){
+          remove=1;
+          break;
+        }
+      }
       for(int i=0; i<next.na; i++)
         next.atoms[i]=current.atoms[i];
-      MultiindexVector next_avail;
-      next_avail.reserve(reactions.size());
-      for (auto itr = reactions.begin(); itr != reactions.end(); ++itr) {
-        int remove=0;
-        Multiindex next(current.na, current.ns);
-        for(int i=0; i<next.ns; i++) {
-          next.species[i]=current.species[i]+itr->species[i];
-          if(next.species[i] < 0){
-            remove=1;
-            break;
-          }
-        }
-        if(remove==0 && previously_enumerated.find(next)==previously_enumerated.end()) {
-          stack.push_back(next);
-          previously_enumerated.insert(next);
-        }
-        // else{
-        //   printf("%i\t %i\t\r", stack.size(), previously_enumerated.size());
-        // }
-        // for(int i =0; i < current.ns; i++)
-        //   if(current.species[i] + itr->species[i] < 0) {
-        //     remove=1;
-        //   }
-        // if(remove==0)
-        //   next_avail.push_back(*itr);
+
+      if(remove==0 && previously_enumerated.find(next)==previously_enumerated.end()) {
+        // printMultiindex(next);
+        stack.push_back(next);
+        previously_enumerated.insert(next);
       }
-      // if(previously_enumerated.find(next)==previously_enumerated.end()){
-      //   if(level+1>maxlevel)
-      //     maxlevel=level+1;
-      //   recursive_list(next,next_avail,reactions,previously_enumerated,ret,count,maxlevel,level+1);
-      // }
     }
   }
-
 }
+
 
 static PyObject *rlist_list(PyObject *self, PyObject* args){
     //Parse arguments and store in C arrays
-    PyArrayObject *species, *spatoms, *exclude, *reactions;
-    int ndspecies, ndspatoms, ndexclude, ndreactions;
-    npy_intp *dspecies, *dspatoms, *dexclude, *dreactions;
-    long int *speciesptr, *spatomsptr, *excludeptr, *reactionsptr;
-    if (!PyArg_ParseTuple(args, "O!O!O!O!", &PyArray_Type, &species, &PyArray_Type, &spatoms,  &PyArray_Type, &exclude, &PyArray_Type, &reactions)) return NULL;
+    PyArrayObject *species, *exclude, *reactions;
+    int ndspecies, ndexclude, ndreactions;
+    npy_intp *dspecies, *dexclude, *dreactions;
+    long int *speciesptr, *excludeptr, *reactionsptr;
+    if (!PyArg_ParseTuple(args, "O!O!O!", &PyArray_Type, &species,  &PyArray_Type, &exclude, &PyArray_Type, &reactions)) return NULL;
     ndspecies = PyArray_NDIM(species);
-    ndspatoms = PyArray_NDIM(spatoms);
     ndexclude = PyArray_NDIM(exclude);
     ndreactions = PyArray_NDIM(reactions);
     dspecies = PyArray_DIMS(species);
-    dspatoms = PyArray_DIMS(spatoms);
     dexclude = PyArray_DIMS(exclude);
     dreactions = PyArray_DIMS(reactions);
-    if(!(ndspecies==1 && ndspatoms==2 && ndexclude == 1)){
+    if(!(ndspecies==1 && ndexclude == 1)){
       printf("Bad input; atoms must be one dimensional, exclude must be 1 dimensional, spatoms must be 2 dimensional, and the size of the second dimension of spatoms must equal the size of atoms.");
       return NULL;
     }
     speciesptr = (long int *)PyArray_DATA(species);
-    spatomsptr = (long int *)PyArray_DATA(spatoms);
     excludeptr = (long int *)PyArray_DATA(exclude);
     reactionsptr = (long int *)PyArray_DATA(reactions);
-    long int ns=dspatoms[0]; //num species
-    long int na=dspatoms[1]; //num atoms
+    long int ns=dreactions[1]; //num species
+    long int na=0; //num atoms
     long int ne=dexclude[0]; //num atoms
-    long int nr=dreactions[1]; //num atoms
+    long int nr=dreactions[0]; //num atoms
+    // printf("%i %i\n", ns,nr);
     MultiindexSet previously_enumerated;
-    MultiindexVector avail, allreac, list;
+    MultiindexVector allreac;
     //initialize the current multiindex with zeros and the number of atoms
     Multiindex current(na, ns);
     for (int i=0; i<na; i++)
@@ -108,47 +80,38 @@ static PyObject *rlist_list(PyObject *self, PyObject* args){
     for (int i=0; i<ns; i++){
       current.species[i]=speciesptr[i];
       for (int j=0; j<na; j++)
-        current.atoms[j]+=spatomsptr[na*i+j]*speciesptr[i];
+        current.atoms[j]+=0;
     }
-
     //populate reactions and avail with available reactions
     for (int i =0; i<nr; i++){
       Multiindex add(na, ns);
       for(int j =0; j<na; j++)
         add.atoms[j]=0;
       for(int j=0; j<ns; j++)
-        add.species[j]=reactionsptr[nr*j+i];
+        add.species[j]=reactionsptr[ns*i+j];
       allreac.push_back(add);
-      int remove=0;
-      for(int i =0; i < current.ns; i++)
-        if(current.species[i] + add.species[i] < 0)
-          remove=1;
-      if(remove==0)
-        avail.push_back(add);
+      // printMultiindex(add);
     }
 
-    long int count=0;
-    long int maxlevel=0;
-    recursive_list(current, avail, allreac, previously_enumerated, list, count, maxlevel, 0);
+    recursive_list(current, allreac, previously_enumerated);
 
     //Populate the numpy output arrays with the information to return to Python
     // long int* data = new long int[list.size()*ns];
-    long int* data = (long int*)malloc(list.size()*ns*sizeof(long int));
+    int dim=previously_enumerated.size();
+    long int* data = (long int*)malloc(dim*ns*sizeof(long int));
     int i=0;
-    for (auto itr = list.begin(); itr != list.end(); ++itr) {
+    for (auto itr = previously_enumerated.begin(); itr != previously_enumerated.end(); ++itr) {
       for(int j=0; j<ns; j++){
         data[ns*i+j] = itr->species[j];
       }
       i++;
     }
     long int retdims[2];
-    retdims[0]=list.size();
+    retdims[0]=dim;
     retdims[1]=ns;
     PyObject *ret0 = PyArray_SimpleNewFromData (2,retdims,NPY_LONG,data);
-    PyObject *ret=Py_BuildValue("(Oii)",ret0,count,maxlevel);
-    // Py_INCREF(ret0);
-    Py_INCREF(ret);
-    return ret;
+    Py_INCREF(ret0);
+    return ret0;
 }
 
 static PyMethodDef rlist_methods[] = {{"list", (PyCFunction)rlist_list, METH_VARARGS, "Return the recursive list."}, {NULL, NULL}};
