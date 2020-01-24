835b7e50a004b4e0c251690171d1d52792667796
znicolaou <zack.nicolaou@gmail.com>
2020-01-24 10:11:24 -0600

    updated plots

--- rlistmodule.c
+++ rlistmodule.c
@@ -13,7 +13,8 @@ static void printMultiindex (Multiindex mult) {
   printf("\n");
 }
 
-static void recursive_list(Multiindex current, MultiindexVector reactions, MultiindexSet &previously_enumerated){
+// static void recursive_list(Multiindex current, MultiindexVector reactions, MultiindexSet &previously_enumerated){
+static void recursive_list(Multiindex current, long int *reactionsptr, int ns, int nr, MultiindexSet &previously_enumerated){
   MultiindexVector stack;
   stack.push_back(current);
   previously_enumerated.insert(current);
@@ -23,18 +24,21 @@ static void recursive_list(Multiindex current, MultiindexVector reactions, Multi
     current=stack.back();
     // printf("%i\t%i\t\r",stack.size(),previously_enumerated.size());
     stack.pop_back();
-    for (auto itr = reactions.begin(); itr != reactions.end(); ++itr) {
+    //since reactions has fixed length, we could do a pragma omp parallel here
+    // for (auto itr = reactions.begin(); itr != reactions.end(); ++itr) {
+    for (int j =0; j < nr; j++) {
       Multiindex next(current.na, current.ns);
       remove=0;
       for(int i=0; i<next.ns; i++){
-        next.species[i]=current.species[i]+itr->species[i];
+        // next.species[i]=current.species[i]+itr->species[i];
+        next.species[i]=current.species[i]+reactionsptr[ns*i+j];
         if(next.species[i] < 0){
           remove=1;
           break;
         }
       }
-      for(int i=0; i<next.na; i++)
-        next.atoms[i]=current.atoms[i];
+      // for(int i=0; i<next.na; i++)
+      //   next.atoms[i]=current.atoms[i];
 
       if(remove==0 && previously_enumerated.find(next)==previously_enumerated.end()) {
         // printMultiindex(next);
@@ -90,7 +94,8 @@ static PyObject *rlist_list(PyObject *self, PyObject* args){
       // printMultiindex(add);
     }
 
-    recursive_list(current, allreac, previously_enumerated);
+    // recursive_list(current, allreac, previously_enumerated);
+    recursive_list(current, reactionptr, ns, nr, previously_enumerated);
 
     //Populate the numpy output arrays with the information to return to Python
     int dim=previously_enumerated.size();
