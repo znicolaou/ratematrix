7281d5c0a4fd993c5d6232fe64d1e0d1643010ef
znicolaou <zack.nicolaou@gmail.com>
2020-04-06 16:49:05 -0500

    updated plots

--- scripts/runtimes3.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-Natoms0=3
-Natoms1=12
-filebase=data/runtimes2
-for i in `seq $Natoms0 $Natoms1`; do
-  echo $i
-  echo "./ratematrix.py --atoms $i $i $i --filebase $filebase --plot 0 --calculate 0 "
-  ./ratematrix.py --atoms $i $i $i --filebase $filebase --plot 0 --calculate 0
-done
